4c2eacd54ae6bf3c73f264afedb2928e9553a4e7
REPLYNET\anto.caputo <anto.caputo@reply.it>
2025-03-12 15:43:22 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void readFile(String path){
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeFile(String filePath, String content) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
